f41d51568492b9df6a17e294c8db96ff0e3ee2e8
NicolasMarinkovic-dev <nicolas.marinkovic01@gmail.com>
2025-06-16 14:24:58 -0300

    Actualizados los .bat con el archivo.asm

--- proyecto-dino/scripts/dinotd.bat
+++ proyecto-dino/scripts/dinotd.bat
@@ -4,9 +4,10 @@ tasm /zi logic.asm
 tasm /zi dino.asm
 tasm /zi score.asm
 tasm /zi sprite.asm
-tasm libsp.asm
+tasm /zi libsp.asm
+tasm /zi archivo.asm
 tasm /zi menu.asm
 
-tlink /v main.obj espera.obj logic.obj dino.obj score.obj sprite.obj libsp.obj menu.obj
+tlink /v main.obj espera.obj logic.obj dino.obj score.obj sprite.obj libsp.obj archivo.obj menu.obj
 
 td main
