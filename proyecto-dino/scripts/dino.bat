8176b145bf4b63fa072f186d25f00f5494231a2c
matias-ortubia <matiortubia@gmail.com>
2025-06-16 04:55:59 -0300

    Agrego score
    TODO: que sume de a uno, permitir 3 digitos

--- proyecto-dino/scripts/dino.bat
+++ proyecto-dino/scripts/dino.bat
@@ -6,7 +6,8 @@ tasm score.asm
 tasm sprite.asm
 tasm libsp.asm
 tasm menu.asm
+tasm score.asm
 
-tlink main.obj espera.obj logic.obj dino.obj score.obj sprite.obj libsp.obj menu.obj
+tlink main.obj espera.obj logic.obj dino.obj score.obj sprite.obj libsp.obj menu.obj score.obj
 
 main.exe
