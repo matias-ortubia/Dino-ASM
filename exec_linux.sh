2796d89ac505b608ed4f697a74f5ce3a9fe5d6e0
matias-ortubia <matiortubia@gmail.com>
2025-06-19 23:53:30 -0300

    update script ubuntu

--- exec_linux.sh
+++ exec_linux.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Script para copiar archivos del proyecto dino en Ubuntu
+# Cambiar las rutas si no funciona
 
-mkdir -p ~/Documents/TASM
-
-cp proyecto-dino/src/intRand.asm ~/Documents/TASM/intRand.asm
-cp proyecto-dino/src/main.asm ~/Documents/TASM/main.asm
-cp proyecto-dino/src/espera.asm ~/Documents/TASM/espera.asm
-cp proyecto-dino/src/logic.asm ~/Documents/TASM/logic.asm
-cp proyecto-dino/src/dino.asm ~/Documents/TASM/dino.asm
-cp proyecto-dino/src/score.asm ~/Documents/TASM/score.asm
-cp proyecto-dino/src/sprite.asm ~/Documents/TASM/sprite.asm
-cp proyecto-dino/src/libsp.asm ~/Documents/TASM/libsp.asm
-cp proyecto-dino/src/archivo.asm ~/Documents/TASM/archivo.asm
-cp proyecto-dino/src/menu.asm ~/Documents/TASM/menu.asm
-cp proyecto-dino/docs/dino.conf ~/Documents/TASM/dino.conf
-cp proyecto-dino/scripts/dino.bat ~/Documents/TASM/dino.bat
-cp proyecto-dino/scripts/dinotd.bat ~/Documents/TASM/dinotd.bat
+cp proyecto-dino/src/intRand.asm ~/Documents/tasm/intRand.asm
+cp proyecto-dino/src/main.asm ~/Documents/tasm/main.asm
+cp proyecto-dino/src/espera.asm ~/Documents/tasm/espera.asm
+cp proyecto-dino/src/logic.asm ~/Documents/tasm/logic.asm
+cp proyecto-dino/src/dino.asm ~/Documents/tasm/dino.asm
+cp proyecto-dino/src/score.asm ~/Documents/tasm/score.asm
+cp proyecto-dino/src/sprite.asm ~/Documents/tasm/sprite.asm
+cp proyecto-dino/src/libsp.asm ~/Documents/tasm/libsp.asm
+cp proyecto-dino/src/archivo.asm ~/Documents/tasm/archivo.asm
+cp proyecto-dino/src/menu.asm ~/Documents/tasm/menu.asm
+cp proyecto-dino/docs/dino.conf ~/Documents/tasm/dino.conf
+cp proyecto-dino/scripts/dino.bat ~/Documents/tasm/dino.bat
+cp proyecto-dino/scripts/dinotd.bat ~/Documents/tasm/dinotd.bat
 
 # Ejecutar DOSBox con el archivo de configuración
-dosbox -conf ~/Documents/TASM/dino.conf
+dosbox -conf ~/Documents/tasm/dino.conf
